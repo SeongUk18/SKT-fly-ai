b13de25208a69b6a6788ead35de87bc031c758ff
seonguk.han <adulty22@gmail.com>
2024-08-05 17:21:21 +0900

    Feat: GPT API Azure 사용 예제

---
+++ AI 융합 프로그래밍/chatbot/sample-01.py
@@ -0,0 +1,26 @@
+import openai
+
+openai.api_key = ""
+openai.api_version = "2023-05-15"
+
+openai.api_type = "azure"
+openai.azure_endpoint = "https://sktflyai.openai.azure.com/"
+
+subject = input("시의 제목을 입력하세요")
+content = input("시의 내용을 입력하세요")
+
+result = openai.chat.completions.create(
+    model="dev-gpt-35-turbo",
+    temperature=1,  # 1까지만 쓰는걸로
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },  # 우리는 그냥 쓰지만 뒷단에서 gpt에 전부 설정되어 있음
+        {"role": "user", "content": f"시의 주제는 {subject}"},
+        {"role": "user", "content": f"시의 내용은 {content}"},
+        {"role": "user", "content": "이 내용으로 시를 써줘"},
+    ],
+)
+
+print(result.choices[0].message.content)
